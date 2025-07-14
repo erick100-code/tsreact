80a785aa10893191b2b9c9b34ee7efaa41364dd0
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-07-14 19:49:35 -0300

    pages
    rotas adicionadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Twopage from './pages/twopage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/tsreact/",
+    element: <App />,
+  },
+  {
+    path:"/tsreact/pagina2",
+    element: <Twopage />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/twopage.jsx
@@ -0,0 +1,9 @@
+function Twopage() {
+    return(
+        <div>
+            <h1>a segunda pagina esta funcionando</h1>
+        </div>
+    )    
+}
+
+export default Twopage
